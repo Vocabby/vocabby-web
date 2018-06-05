e7ff9caa69d4c926f88657908096a6ea714815bf
Constantine Lebedev <koss.lebedev@gmail.com>
2018-06-05 10:29:55 +0200

    Show placeholders while vocab list is loading

---
+++ src/modules/Vocabs/VocabPlaceholder/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { VocabContainer } from './styled'
+import { Column } from 'components/Generic'
+
+const VocabPlaceholder = () => (
+  <Column width={25}>
+    <VocabContainer>
+    </VocabContainer>
+  </Column>
+)
+
+export default VocabPlaceholder
--- src/modules/Vocabs/index.js
+++ src/modules/Vocabs/index.js
@@ -4,15 +4,21 @@ import { compose, lifecycle } from 'recompose'
 import { connect } from 'react-redux'
 import Hero from './Hero'
 import VocabList from './VocabList'
+import VocabPlaceholder from './VocabPlaceholder'
+import { Container } from 'components/Generic'
 import withLayout from 'hocs/withLayout'
 
 const Vocabs = props => (
   <Fragment>
     <Hero />
-    <VocabList {...props} />
+    {props.isLoading && (
+      <Container>
+        {Array.from({ length: 16 }, (value, key) => <VocabPlaceholder key={key} />)}
+      </Container>
+    )}
+    {!props.isLoading && <VocabList {...props} />}
   </Fragment>
 )
-
 
 export default compose(
   connect(state => state.vocabs, ({ vocabs: { loadAsync } }) => ({ loadAsync })),
